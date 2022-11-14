c5fb44b84ff33903f417c1dbbee52eff32b3a02f
beckele94 <ulysseguillot@gmail.com>
2022-11-14 17:54:07 +0100

    ajout du crud de la classe Ticket

--- app/src/Controller/TicketController.php
+++ app/src/Controller/TicketController.php
@@ -3,40 +3,76 @@
 namespace App\Controller;
 
 use App\Entity\Ticket;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Form\Ticket1Type;
+use App\Repository\TicketRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-#[Route('/tickets')]
+#[Route('/ticket')]
 class TicketController extends AbstractController
 {
-    #[Route('/', name: "tickets_list")]
-    public function index(ManagerRegistry $doctrine): Response{
-
-        $tickets = $doctrine->getRepository(Ticket::class)->findAll();
-        // dd($tickets);
-        
-        return $this->render ('tickets/list.html.twig', [
-            'tickets' => $tickets,
+    #[Route('/', name: 'app_ticket_index', methods: ['GET'])]
+    public function index(TicketRepository $ticketRepository): Response
+    {
+        return $this->render('ticket/index.html.twig', [
+            'tickets' => $ticketRepository->findAll(),
         ]);
-
     }
 
-    #[Route('/{ticketId}', name: "tickets_show")]
-    public function show(int $ticketId, ManagerRegistry $doctrine): Response
+    #[Route('/new', name: 'app_ticket_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TicketRepository $ticketRepository): Response
     {
-        $ticket = $doctrine->getRepository(Ticket::class)->find($ticketId);
+        $ticket = new Ticket();
+        $form = $this->createForm(Ticket1Type::class, $ticket);
+        $form->handleRequest($request);
 
-        if (!$ticket){
-            throw $this->createNotFoundException(
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ticketRepository->save($ticket, true);
 
-            );
+            return $this->redirectToRoute('app_ticket_index', [], Response::HTTP_SEE_OTHER);
         }
-        
-        return $this->render ('tickets/show.html.twig', [
-            'ticket' => $ticket
+
+        return $this->renderForm('ticket/new.html.twig', [
+            'ticket' => $ticket,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_ticket_show', methods: ['GET'])]
+    public function show(Ticket $ticket): Response
+    {
+        return $this->render('ticket/show.html.twig', [
+            'ticket' => $ticket,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_ticket_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ticket $ticket, TicketRepository $ticketRepository): Response
+    {
+        $form = $this->createForm(Ticket1Type::class, $ticket);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ticketRepository->save($ticket, true);
+
+            return $this->redirectToRoute('app_ticket_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ticket/edit.html.twig', [
+            'ticket' => $ticket,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_ticket_delete', methods: ['POST'])]
+    public function delete(Request $request, Ticket $ticket, TicketRepository $ticketRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ticket->getId(), $request->request->get('_token'))) {
+            $ticketRepository->remove($ticket, true);
+        }
+
+        return $this->redirectToRoute('app_ticket_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ app/src/Form/TicketType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label', TextType::class,[
+                'attr' => [
+                    'placeholder' => 'nom '
+                ]
+            ])
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
